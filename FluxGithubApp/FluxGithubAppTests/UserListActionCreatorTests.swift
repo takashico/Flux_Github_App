cd67e44fbdfea4ffa0b5eeea82530ae077867a84
takashico <s1190089@gmail.com>
2022-07-28 17:14:16 +0900

    add: userListActionCreatorでの他アクションテストを追加

--- FluxGithubApp/FluxGithubAppTests/UserListActionCreatorTests.swift
+++ FluxGithubApp/FluxGithubAppTests/UserListActionCreatorTests.swift
@@ -32,19 +32,24 @@ class UserListActionCreatorTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        
         dependency = Dependency()
     }
     
     func testFirstFetchUserList() {
-        let resultUsers: [User] = [User.mock1()]
-        let expect = expectation(description: "waiting UserListAction.firstFetched")
+        let fetchedExpect = expectation(description: "waiting UserListAction.firstFetched")
+        let fetchStartExpect = expectation(description: "waiting UserListAction.firstFetchStart")
+        let fetchEndExpect = expectation(description: "waiting UserListAction.firstFetchEnd")
         
         let disposable = dependency.dispatcher.register { action in
             switch action as? UserListAction {
             case let .firstFetched(_, users):
+                let resultUsers: [User] = [User.mock1()]
                 XCTAssertEqual(users, resultUsers)
-                expect.fulfill()
+                fetchedExpect.fulfill()
+            case .firstFetchStart:
+                fetchStartExpect.fulfill()
+            case .firstFetchEnd:
+                fetchEndExpect.fulfill()
             default:
                 break
             }
@@ -52,7 +57,15 @@ class UserListActionCreatorTests: XCTestCase {
         
         dependency.actionCreator.firstFetchUserList(perPage: 2)
         
-        wait(for: [expect], timeout: 1.0)
+        wait(
+            for: [
+                fetchStartExpect,
+                fetchEndExpect,
+                fetchedExpect
+            ],
+            timeout: 1.0,
+            enforceOrder: true
+        )
         
         disposable.dispose()
     }
@@ -80,14 +93,20 @@ class UserListActionCreatorTests: XCTestCase {
     }
     
     func testMoreFetchUserList() {
-        let resultUsers: [User] = [User.mock2()]
-        let expect = expectation(description: "waiting UserListAction.moreFetched")
+        let fetchedExpect = expectation(description: "waiting UserListAction.moreFetched")
+        let fetchStartExpect = expectation(description: "waiting UserListAction.moreFetchStart")
+        let fetchEndExpect = expectation(description: "waiting UserListAction.moreFetchEnd")
         
         let disposable = dependency.dispatcher.register { action in
             switch action as? UserListAction {
             case let .moreFetched(_, users):
+                let resultUsers: [User] = [User.mock2()]
                 XCTAssertEqual(users, resultUsers)
-                expect.fulfill()
+                fetchedExpect.fulfill()
+            case .moreFetchStart:
+                fetchStartExpect.fulfill()
+            case .moreFetchEnd:
+                fetchEndExpect.fulfill()
             default:
                 break
             }
@@ -95,7 +114,15 @@ class UserListActionCreatorTests: XCTestCase {
         
         dependency.actionCreator.moreFetchUserList(since: 1, perPage: 2)
         
-        wait(for: [expect], timeout: 1.0)
+        wait(
+            for: [
+                fetchStartExpect,
+                fetchEndExpect,
+                fetchedExpect
+            ],
+            timeout: 1.0,
+            enforceOrder: true
+        )
         
         disposable.dispose()
     }
---
+++ FluxGithubApp/FluxGithubAppTests/UserListStoreTests.swift
@@ -0,0 +1,8 @@
+//
+//  UserListStoreTests.swift
+//  FluxGithubAppTests
+//
+//  Created by takashico on 2022/07/28.
+//
+
+import Foundation
